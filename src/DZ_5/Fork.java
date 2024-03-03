3f2081bfcbb07b85677a66c8fdfab65cba0da6cb
AbashinAE <a.abashin@list.ru>
2024-03-03 12:47:51 +0300

    add DZ_5

---
+++ src/DZ_5/Fork.java
@@ -0,0 +1,11 @@
+package DZ_5;
+
+public class Fork {
+    private boolean using;
+    public boolean isUsing() {
+        return using;
+    }
+    public void setUsing(boolean using) {
+        this.using = using;
+    }
+}
